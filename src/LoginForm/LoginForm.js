a5e9fc6dec0cfbb6d87ba46992fb625a42ee269e
Kate Kuzkina <kate.kuzkina@gmail.com>
2018-02-10 18:49:49 +0300

    added error handling to login form

--- src/LoginForm/LoginForm.js
+++ src/LoginForm/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form } from 'semantic-ui-react'
+import { Form, Message, Button } from 'semantic-ui-react'
 import { connect } from 'react-redux';
 import { loginUser } from '../actions/user';
 import { checkUserPassword } from '../utils/apiLogin';
@@ -7,6 +7,9 @@ import { checkUserPassword } from '../utils/apiLogin';
 
 
 export class LoginForm extends Component {
+    state = {
+        secondAttempt: false
+    }
 
     login = (event) => {
         event.preventDefault();
@@ -15,16 +18,22 @@ export class LoginForm extends Component {
         checkUserPassword(user.username, user.password).then(() => {
             this.props.loginUser(user.username);
             this.props.onLogin();
-        });
+        }).catch(() => this.setState({ secondAttempt: true }));
     }
 
     render() {
 
         return (
-            <Form onSubmit={this.login}>
+            <Form onSubmit={this.login} error>
+                {this.state.secondAttempt
+                    ? <Message error header='Incorrect password' content='Please, try again' />
+                    : null}
                 <Form.Input type='text' placeholder='Username' name='username' />
-                <Form.Input type='password' placeholder='Password' name='password' />
-                <Form.Button color='green'>Login</Form.Button>
+                {this.state.secondAttempt
+                    ? <Form.Input type='password' placeholder='Password' name='password' error />
+                    : <Form.Input type='password' placeholder='Password' name='password' />}
+
+                <Button color='green'>Login</Button>
             </Form>
         )
     }
